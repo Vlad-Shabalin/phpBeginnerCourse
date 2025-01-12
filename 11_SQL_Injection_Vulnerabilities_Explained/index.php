d2cc432eeaac13fd9c0a33f965ddcc467709e72d
Vlad Shabalin <mac@macs-MacBook-Air.local>
2025-01-12 18:49:02 +0200

    11_SQL_Injection_Vulnerabilities_Explained

--- 11_SQL_Injection_Vulnerabilities_Explained/index.php
+++ 11_SQL_Injection_Vulnerabilities_Explained/index.php
@@ -8,6 +8,9 @@ $config = require('config.php');
 
 $db = new Database($config['database'], 'root', 1111111111);
 
-$posts = $db->query("select * from posts")->fetchAll();
+$id = $_GET['id'];
+$query = "select * from posts where id = ?";
+
+$posts = $db->query($query, [$id])->fetch();
 
 dd($posts);
